452fcc1a05f8f885cb06e5622997b7af43434990
Ryan Weston <rwest88.va@gmail.com>
2018-03-24 15:53:48 -0400

    line formatting and app.js created

---
+++ assets/javascript/app.js
@@ -0,0 +1,37 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyChvFGY3IOW8HQf30alGRKO6_Psp2Un7cs",
+  authDomain: "spotify-alarm-1521758993850.firebaseapp.com",
+  databaseURL: "https://spotify-alarm-1521758993850.firebaseio.com",
+  projectId: "spotify-alarm-1521758993850",
+  storageBucket: "",
+  messagingSenderId: "368791517227"
+};
+
+firebase.initializeApp(config);
+
+var $collapsible = $('#collapseExample');
+
+$('#datetimepicker3').datetimepicker({
+  format: 'LT'
+});
+$('#alarm-form').on('submit', function(evt) {
+  evt.preventDefault();
+  $collapsible.collapse('hide');
+  $('#alarm-set-msg').addClass('show');
+});
+  
+var d = new Date();
+var timeHours = d.getHours();
+var timeMinutes = d.getMinutes();
+var timeSeconds = d.getSeconds();
+
+// if (timeHours > 12) {
+  
+// }
+
+// $('#submit-btn').on('click', function () {
+//   if ()
+// });
+
+console.log(`${timeHours}:${timeMinutes}`);
